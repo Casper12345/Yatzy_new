c7e49e8cab746099cb3876aebad6283779a747e1
Casper12345 <caspertellerup@hotmail.com>
2016-08-07 13:09:16 +0200

    Changelist

---
+++ Control.py
@@ -0,0 +1,14 @@
+
+
+def main():
+    print 'test'
+
+
+
+
+
+
+
+
+
+
---
+++ Hook.py
@@ -0,0 +1,9 @@
+""" This is the start up program that hooks up to the control script """
+
+# Import statments
+
+import Control
+
+if __name__ == '__main__':
+    Control.main()
+
